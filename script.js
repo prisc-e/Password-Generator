a4f90a5e18187fc77072458bc3fe799b94b05820
Priscilla <egli.priscilla@gmail.com>
2021-11-02 08:53:37 -0500

    fixed input validation through improved if/else prompts

--- script.js
+++ script.js
@@ -3,74 +3,85 @@
 // Assignment code here
 
 function generatePassword(passwordText){
-  //var confirmSkip = window.confirm("Are you sure you'd like to quit?");
-  //if (confirmSkip) 
-  //window.alert(playerName + " has decided to skip this fight. Goodbye!");
-  var promptLength = window.prompt("How long should the password length (must be at least 8 characters and no more than 128 characters)?"); 
   
-  var specialArr = ["~", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "+", "="];
-  var lowerArr = ["a", "b","c", "d","e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s","t", "u", "v", "w", "x", "y", "z"]
-  var upperArr = ["A", "B","C", "D","E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S","T", "U", "V", "W", "X", "Y", "Z"]
-  var passString = " ";
-  
-  if (promptLength >= 8 && promptLength <= 128){
-
-  }
-  else {
+  var promptLength = window.prompt("How long should the password be (must be between 8 and 128 characters)?"); 
+  if (promptLength < 8 || promptLength > 128 || isNaN(promptLength) == true){
+    
     window.alert(promptLength + " is not a number between 8 and 128. Try again");
     promptLength = window.prompt("How long should the password be (must be at least 8 characters and no more than 128 characters)?");
   }
-  var promptLowercase = window.prompt("Do you want to include lowercase letters? (yes or no)");
-  var promptUppercase = window.prompt("Do you want to include uppercase letters? (yes or no)");
-  var promptNumeric = window.prompt("Do you want to include numeric characters? (yes or no)");
-  var promptSpecial = window.prompt("Do you want to include special characters?(yes or no)");
 
 
-  if (promptSpecial === "no" && promptLowercase === "no" && promptUppercase === "no" && promptNumeric === "no"){
-    window.alert("You must choose at least one character type. Try again.")
+  let specialArr = ["~", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "+", "="];
+  let lowerArr = ["a", "b","c", "d","e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s","t", "u", "v", "w", "x", "y", "z"]
+  let upperArr = ["A", "B","C", "D","E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S","T", "U", "V", "W", "X", "Y", "Z"]
+  let passString = " ";
+  
+  let promptLowercase = " "; 
+  let promptUppercase = " ";
+  let promptNumeric = " ";
+  let promptSpecial = " ";
+  
+ 
+  promptLowercase = window.prompt("Do you want to include lowercase letters? (yes or no)");
+    if (promptLowercase === "no" || promptLowercase === "yes" || promptLowercase === "No" || promptLowercase === "Yes"){
+    } else {
+      window.alert("Answer must be yes or no. Try again");
+      promptLowercase = window.prompt("Do you want to include lowercase letters? (yes or no)");
+    }
+  
+  promptUppercase = window.prompt("Do you want to include uppercase letters? (yes or no)");
+    if (promptUppercase === "no" || promptUppercase === "yes" || promptUppercase === "No" || promptUppercase === "Yes"){
+        }else {
+        window.alert("Answer must be yes or no. Try again");
+        promptUppercase = window.prompt("Do you want to include uppercase letters? (yes or no)");
+    }
 
-    promptLowercase = window.prompt("Do you want to include lowercase letters? (yes or no)");
-    promptUppercase = window.prompt("Do you want to include uppercase letters? (yes or no)");
-    promptNumeric = window.prompt("Do you want to include numeric characters? (yes or no)");
-    promptSpecial = window.prompt("Do you want to include special characters?(yes or no)");
+  promptNumeric = window.prompt("Do you want to include numeric characters? (yes or no)");
+    if (promptNumeric === "no" || promptNumeric === "yes" || promptNumeric === "No" || promptNumeric === "Yes"){
+    } else {
+      window.alert("Answer must be yes or no. Try again");
+      promptNumeric = window.prompt("Do you want to include numeric characters? (yes or no)");
+    }
 
-  }
-  else if (promptLowercase != "no" || promptLowercase != "yes"){
-    window.alert("Answer must me yes or no. Try again");
-    promptLowercase = window.prompt("Do you want to include lowercase letters? (yes or no)");
-  }
-  else if (promptUppercase != "no" || promptUppercase != "yes"){
-    window.alert("Answer must me yes or no. Try again");
-    promptUppercase = window.prompt("Do you want to include uppercase letters? (yes or no)");
-  }   
-  else if (promptNumeric != "no" || promptNumeric != "yes"){
-    window.alert("Answer must me yes or no. Try again");
-    promptNumeric = window.prompt("Do you want to include numeric characters? (yes or no)");
-  }
-  else if (promptSpecial != "no" || promptSpecial != "yes"){
-    window.alert("Answer must me yes or no. Try again");
-    promptSpecial = window.prompt("Do you want to include special characters?(yes or no)");
-  }
+  promptSpecial = window.prompt("Do you want to include special characters?(yes or no)");
+    if (promptSpecial === "no" || promptSpecial === "yes" || promptSpecial === "No" || promptSpecial === "Yes"){
+    } else {
+      window.alert("Answer must be yes or no. Try again");
+      promptSpecial = window.prompt("Do you want to include special characters?(yes or no)");
+    }
+        //if all no's
+    if (promptSpecial == "no" && promptLowercase == "no" && promptUppercase == "no" && promptNumeric == "no"){
+       window.alert("You must choose at least one character type. Try again.");
 
-  while (promptLength > passString.length){
+      promptLowercase = window.prompt("Do you want to include lowercase letters? (yes or no)");
+      promptUppercase = window.prompt("Do you want to include uppercase letters? (yes or no)");
+      promptNumeric = window.prompt("Do you want to include numeric characters? (yes or no)");
+      promptSpecial = window.prompt("Do you want to include special characters?(yes or no)");
 
-    if (promptSpecial === "yes"){
+    }
+  
+  
+  //loop to select random char from arrays
+  while (promptLength > passString.length-1){
+
+    if (promptSpecial == "yes"){
       
-     var specialChar = specialArr[Math.floor(Math.random() * specialArr.length)];
+     let specialChar = specialArr[Math.floor(Math.random() * specialArr.length)];
     passString += specialChar.toString();
     console.log(specialChar);
     }
-    if (promptUppercase === "yes"){
-       var upperChar = upperArr[Math.floor(Math.random() * upperArr.length)];
+    if (promptUppercase == "yes"){
+       let upperChar = upperArr[Math.floor(Math.random() * upperArr.length)];
        passString += upperChar.toString();
        console.log(upperChar);
     }
-    if (promptLowercase === "yes") {
-      var lowerChar = lowerArr[Math.floor(Math.random() * lowerArr.length)];
+    if (promptLowercase == "yes") {
+      let lowerChar = lowerArr[Math.floor(Math.random() * lowerArr.length)];
       passString += lowerChar.toString();
       console.log(lowerChar);
     }
-    if (promptNumeric === "yes"){  
+    if (promptNumeric == "yes"){  
       passString += Math.floor(Math.random()*10).toString();
     }
   console.log(passString);
@@ -83,14 +94,14 @@ return passwordText;
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  let password = generatePassword();
+  let passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 }
 
 // Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
+let generateBtn = document.querySelector("#generate");
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
